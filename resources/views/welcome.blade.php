97d172eb3053a2d589e437085a74835b949bcd69
Edwin Castro <141517335+3dwinC4astro@users.noreply.github.com>
2024-10-13 16:58:34 -0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -7,11 +7,11 @@
         <meta name="author" content="" />
         <title>CINE-MARIANA</title>
         <!-- Favicon-->
-        <link rel="icon" type="image/x-icon" href="{{ asset('assets/favicon.ico') }}" />
+        <link rel="icon" type="image/x-icon" href="{{ secure_asset('assets/favicon.ico') }}" />
         <!-- Bootstrap icons-->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.5.0/font/bootstrap-icons.css" rel="stylesheet" />
         <!-- Core theme CSS (includes Bootstrap)-->
-        <link href="{{ asset('css/styles.css') }}" rel="stylesheet" />
+        <link href="{{ secure_asset('css/styles.css') }}" rel="stylesheet" />
     </head>
     <body>
         <!-- Navigation-->
@@ -108,6 +108,6 @@
         <!-- Bootstrap core JS-->
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"></script>
         <!-- Core theme JS-->
-        <script src="{{ asset('js/scripts.js') }}"></script>
+        <script src="{{ secure_asset('js/scripts.js') }}"></script>
     </body>
 </html>
